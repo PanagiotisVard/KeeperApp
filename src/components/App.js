20ab0bc8cb897d66859c296aa968126bfb5475f9
PanagiotisVard <panagiotis.vardaris@uoi.gr>
2023-07-21 13:59:58 +0300

    Added addNote and deleteNote functions.

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,12 @@
 //We are importing React library in every file because this library is very important
-import React from "react";
+import React, { useState } from "react";
 //Here we import Header.js file as "Header" to have access into the component Header.js file has
 import Header from "./Header";
 //Here we import Footer.js file as "Footer" to have access into the component Footer.js file has
 import Footer from "./Footer";
 //Here we import Note.js file as "Note" to have access into the component Note.js file has
 import Note from "./Note";
-import notes from "../notes";
+import CreateArea from "./CreateArea";
 
 //We created this function to create a Note for each object the notes.js file has.
 //This function will be called by map() as many times as the number of our objects inside the  list
@@ -21,24 +21,42 @@ import notes from "../notes";
 //   );
 // }
 
-const App = () => {
+function App(){
   //In order to use the <Header /> function we must import first.
+  const [notes, setNotes] = useState([]);
+
+  function addNote(newNote) {
+    setNotes(prevNotes => {
+      return [...prevNotes, newNote];
+    });
+  }
+
+  function deleteNote(id) {
+    setNotes(prevNotes => {
+      return prevNotes.filter((noteItem, index) => {
+        return index !== id;
+      });
+    });
+  }
+
   return (
     <div>
-      <Header/>
-      {/* We are scanning the list objects from the file notes.js by using map() function.
-      and we give as an argument the function that we made above createNote() */}
-      {notes.map((noteItem) => (
-        <Note
-          key={noteItem.key}
-          title={noteItem.title}
-          content={noteItem.content}
-        />
-      ))}
-      <Footer/>
+      <Header />
+      <CreateArea onAdd={addNote} />
+      {notes.map((noteItem, index) => {
+        return (
+          <Note
+            key={index}
+            id={index}
+            title={noteItem.title}
+            content={noteItem.content}
+            onDelete={deleteNote}
+          />
+        );
+      })}
+      <Footer />
     </div>
   );
-};
-
+}
 //MUST HAVE AN EXPORT TO GIVE ACCESS IN OTHER FILES
 export default App;
--- src/components/Note.js
+++ src/components/Note.js
@@ -1,16 +1,22 @@
 //We are importing React library in every file because this library is very important
 import React from "react";
 
-
-//This function is being called to create a Note.The "props" argument 
+//This function is being called to create a Note.The "props" argument
 //helps us to have access in data from other files.
-function Note(props){
+function Note(props) {
 
-    return(<div className="note">
-        <h1>{props.title}</h1>
-        <p>{props.content}</p>
+
+  function handleClick() {
+    props.onDelete(props.id)
+  }
+
+  return (
+    <div className="note">
+      <h1>{props.title}</h1>
+      <p>{props.content}</p>
+      <button onClick={handleClick}>DELETE</button>
     </div>
-    );
+  );
 }
 
 export default Note;
